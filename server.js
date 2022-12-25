ecbd9991062563d2c0e1065fbc38d9ab8ff36bc5
aaronk2002 <aaronakj2002@gmail.com>
2022-12-24 19:25:56 -0800

    Attempt to fix Heroku Upload

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-var port_number = server.listen(process.env.PORT || 3000);
+var port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port_number, () => {
+app.listen(port, () => {
     console.log('Hello World app successfully launched on port %s', port)
 })
